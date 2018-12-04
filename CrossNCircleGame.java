f47f729bd259d1afa07cf73b5059735614011a08
mariuszbyahoo <43074935+mariuszbyahoo@users.noreply.github.com>
2018-12-04 20:36:51 +0100

    Redundancja kodu

---
+++ CrossNCircleGame.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class CrossNCircleGame {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean game = true;
+        System.out.println("1 -> grasz z komputerem, inna liczba - grasz na gorące krzesło");
+        int choice = input.nextInt();
+        Data data = new Data();
+            while (game = true) {
+                data.printTable();
+                data.move("O");
+                data.printTable();
+                data.checkEnd();
+                if (data.gameOver == true) {
+                    break;
+                }
+                if(choice == 1) {
+                    data.moveAi("X");
+                }else{
+                    data.move("X");
+                }
+                data.printTable();
+                data.checkEnd();
+                if (data.gameOver == true) {
+                    break;
+                }
+            }
+    }
+}

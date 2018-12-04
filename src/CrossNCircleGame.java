e8752eb09c54e637e998dfc36bde0b689d176cc3
mariuszbyahoo <43074935+mariuszbyahoo@users.noreply.github.com>
2018-12-04 20:34:24 +0100

    Dodanie wyboru trybu gry
    Gra z komputerem albo na gorące krzesło

--- src/CrossNCircleGame.java
+++ src/CrossNCircleGame.java
@@ -1,20 +1,43 @@
+import java.util.Scanner;
+
 public class CrossNCircleGame {
-    public static void main (String[]args){
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         boolean game = true;
+        System.out.println("1 -> grasz z komputerem, inna liczba - grasz na gorące krzesło");
+        int choice = input.nextInt();
         Data data = new Data();
-        while(game = true) {
-            data.printTable();
-            data.move("O");
-            data.printTable();
-            data.checkEnd();
-            if(data.gameOver == true){
-                break;
+        if (choice == 1) {
+            while (game = true) {
+                data.printTable();
+                data.move("O");
+                data.printTable();
+                data.checkEnd();
+                if (data.gameOver == true) {
+                    break;
+                }
+                data.moveAi("X");
+                data.printTable();
+                data.checkEnd();
+                if (data.gameOver == true) {
+                    break;
+                }
             }
-            data.moveAi("X");
-            data.printTable();
-            data.checkEnd();
-            if(data.gameOver == true){
-                break;
+        } else {
+            while (game = true) {
+                data.printTable();
+                data.move("O");
+                data.printTable();
+                data.checkEnd();
+                if (data.gameOver == true) {
+                    break;
+                }
+                data.move("X");
+                data.printTable();
+                data.checkEnd();
+                if (data.gameOver == true) {
+                    break;
+                }
             }
         }
     }

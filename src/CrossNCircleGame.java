01eb3fcccb3572fe756e9dc826fadb168045e9b7
mariuszbyahoo <43074935+mariuszbyahoo@users.noreply.github.com>
2018-12-16 16:04:07 +0100

    Dodanie możliwości wyboru typu pionka gry
    Klasa z metodą main()

--- src/CrossNCircleGame.java
+++ src/CrossNCircleGame.java
@@ -3,29 +3,65 @@ import java.util.Scanner;
 public class CrossNCircleGame {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        Scanner input2 = new Scanner(System.in);
         boolean game = true;
+        boolean moveWasDone = false;
         System.out.println("1 -> grasz z komputerem, inna liczba - grasz na gorące krzesło");
         int choice = input.nextInt();
+
         System.out.println("Jak duża plansza?");
         Data data = new Data(input.nextInt());
-            while (game = true) {
-                data.printTable();
-                data.move("O");
-                data.printTable();
-                data.checkEnd();
-                if (data.gameOver == true) {
-                    break;
-                }
-                if(choice == 1) {
-                    data.moveAi("X");
-                }else{
-                    data.move("X");
-                }
-                data.printTable();
-                data.checkEnd();
-                if (data.gameOver == true) {
-                    break;
-                }
+
+        System.out.println("Grasz kólkiem (O) czy krzyżykiem (X)?");
+        boolean proper = false;
+        String type = input2.nextLine();
+        while (!proper) {
+            if (type.equals("O")) {
+                System.out.println("Grasz kółkiem, przygotowanie planszy...");
+                proper = true;
+            } else if (type.equals("X")) {
+                System.out.println("Grasz krzyżykiem, przygotowanie planszy...");
+                proper = true;
+            } else {
+                System.out.println("Zła komenda, podaj jedną WIELKĄ literę; O lub X");
+                proper = false;
             }
+        }
+
+        while (game = true) {
+            data.printTable();
+            while (!moveWasDone) {
+                moveWasDone = data.move(type);
+                data.printTable();
+            }
+            moveWasDone = false;
+            data.checkEnd();
+            if (data.gameOver == true) {
+                break;
+            }
+            if (choice == 1) {
+                if (type.equals("O")){
+                    data.moveAi("X");
+                }else if(type.equals("X")){
+                    data.moveAi("O");
+                }
+            } else {
+                while (!moveWasDone) {
+                    if (type.equals("O")){
+                        moveWasDone = data.move("X");
+                    }else if(type.equals("X")){
+                        moveWasDone = data.move("O");
+                    }
+                    data.printTable();
+                }
+                moveWasDone = false;
+            }
+            data.printTable();
+            data.checkEnd();
+            Data.round++;
+            if (data.gameOver == true) {
+                break;
+            }
+        }
     }
 }

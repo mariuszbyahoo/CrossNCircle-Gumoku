9db06618dffa4451f3fcfc63fcf95bdb99166520
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-01-13 15:00:49 +0100

    Modyfying the checkEndInRow() and checkEndInColumn methods to apply them to Gumoku rules.

--- src/pl/budzisz/mariusz/cross_n_circle/game_modes/GumokuRules.java
+++ src/pl/budzisz/mariusz/cross_n_circle/game_modes/GumokuRules.java
@@ -5,8 +5,6 @@ import pl.budzisz.mariusz.cross_n_circle.figures.Figures;
 
 public class GumokuRules extends GameRules {
 
-    public Data data;
-
     public GumokuRules(Data data) {
         super(data);
     }
@@ -38,27 +36,51 @@ public class GumokuRules extends GameRules {
     }
 
     public boolean checkEndInRow(int rowNumber) {
-        boolean gameOver = true;
-        for (int i = 1; i < data.tab[rowNumber].length; i++) {
+        boolean gameOver = false;
+        for (int i = 4; i < data.tab[rowNumber].length; i++) {
             Figures current = data.tab[rowNumber][i];
             Figures previous = data.tab[rowNumber][i - 1];
-            if (current.equals(Figures.EMPTY) || !current.equals(previous)) {
+            Figures preprevious = data.tab[rowNumber][i - 2];
+            Figures prepreprevious = data.tab[rowNumber][i - 3];
+            Figures preprepreprevious = data.tab[rowNumber][i - 4];
+
+            if(current.equals(Figures.EMPTY) || previous.equals(Figures.EMPTY)
+            || preprevious.equals(Figures.EMPTY) || prepreprevious.equals(Figures.EMPTY)
+            || preprepreprevious.equals(Figures.EMPTY)){
                 gameOver = false;
-                break;
+            }else if (current.equals(previous) && previous.equals(preprevious)
+                    && preprevious.equals(prepreprevious)
+                    && prepreprevious.equals(preprepreprevious)) {
+            gameOver = true;
+            break;
+            } else {
+                gameOver = false;
             }
-        }
+
+            }
         return gameOver;
     }
 
     public boolean checkEndInColumn(int columnNumber) {
-        boolean gameOver = true;
-        for (int i = 1; i < data.tab.length; i++) {
+        boolean gameOver = false;
+        for (int i = 4; i < data.tab.length; i++) {
             Figures current = data.tab[i][columnNumber];
             Figures previous = data.tab[i - 1][columnNumber];
+            Figures preprevious = data.tab[i - 2][columnNumber];
+            Figures prepreprevious = data.tab[i-3][columnNumber];
+            Figures preprepreprevious = data.tab[i - 4][columnNumber];
 
-            if (current.equals(Figures.EMPTY) || !current.equals(previous)) {
+            if(current.equals(Figures.EMPTY) || previous.equals(Figures.EMPTY)
+                    || preprevious.equals(Figures.EMPTY) || prepreprevious.equals(Figures.EMPTY)
+                    || preprepreprevious.equals(Figures.EMPTY)){
                 gameOver = false;
+            }else if (current.equals(previous) && previous.equals(preprevious)
+                    && preprevious.equals(prepreprevious)
+                    && prepreprevious.equals(preprepreprevious)) {
+                gameOver = true;
                 break;
+            } else {
+                gameOver = false;
             }
         }
         return gameOver;

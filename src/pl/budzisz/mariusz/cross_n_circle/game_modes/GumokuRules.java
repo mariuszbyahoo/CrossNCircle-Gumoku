4895aa529a240011dc1790418cd08ad23756eb70
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-01-18 21:18:35 +0100

    There commes a first version of AI Interrupting tactics; Now if 3 foe's figures appears, then AI strikes back, in order to stop the appearing failure. It's a very basic version, acting properly only in the case of setting 3 figures in a row, or in a column.
    BUG: This implementation blocks any further computer actions; when it will stop You from gaining the victory it won't play further, just will attack the same
    place all the time infinitively.

--- src/pl/budzisz/mariusz/cross_n_circle/game_modes/GumokuRules.java
+++ src/pl/budzisz/mariusz/cross_n_circle/game_modes/GumokuRules.java
@@ -46,4 +46,5 @@ public class GumokuRules extends GameRules {
         }
         return true;
     }
+
 }
--- src/pl/budzisz/mariusz/cross_n_circle/players/PlayerAI.java
+++ src/pl/budzisz/mariusz/cross_n_circle/players/PlayerAI.java
@@ -11,24 +11,32 @@ import static pl.budzisz.mariusz.cross_n_circle.Data.round;
 public class PlayerAI extends Player {
 
     GameRules gameRules;
+    private int possibleX;
+    private int possibleY;
+
+    Figures foesFigure = figure.equals(Figures.CROSS) ? Figures.CIRCLE : Figures.CROSS;
     public PlayerAI (Figures figure , Data data, GameRules gameRules) {
         super(figure , data);
         this.gameRules = gameRules;
     }
 
     @Override
-    public void move(){
+    public void move() {
         if (round == 2) {
             if (data.tab[0][0].equals(Figures.EMPTY)) {
                 data.tab[0][0] = figure;
-            } else if (data.tab[data.tab.length-1][data.tab.length-1].equals(Figures.EMPTY)) {
-                data.tab[data.tab.length-1][data.tab.length-1] = figure;
-            } else if (data.tab[0][data.tab.length-1].equals(Figures.EMPTY)) {
-                data.tab[0][data.tab.length-1] = figure;
-            } else if (data.tab[data.tab.length-1][0].equals(Figures.EMPTY)) {
-                data.tab[data.tab.length-1][0] = figure;
+            } else if (data.tab[data.tab.length - 1][data.tab.length - 1].equals(Figures.EMPTY)) {
+                data.tab[data.tab.length - 1][data.tab.length - 1] = figure;
+            } else if (data.tab[0][data.tab.length - 1].equals(Figures.EMPTY)) {
+                data.tab[0][data.tab.length - 1] = figure;
+            } else if (data.tab[data.tab.length - 1][0].equals(Figures.EMPTY)) {
+                data.tab[data.tab.length - 1][0] = figure;
             }
-        } else {
+        }else if (is3InAColumn(data)) {
+            counterAttack(possibleX, possibleY, data);
+        }else if (is3InARow(data)){
+            counterAttack(possibleX,possibleY,data);
+        }else {
             for (int i = 0; i < data.tab.length; i++) {
                 for (int j = 0; j < data.tab[i].length; j++) {
                     if (data.tab[i][j].equals(Figures.EMPTY)) {
@@ -40,7 +48,7 @@ public class PlayerAI extends Player {
                         } else {
                             data.tab[i][j] = Figures.EMPTY;
                         }
-                        data.tab[i][j] = figure.equals(Figures.CROSS) ? Figures.CIRCLE : Figures.CROSS;
+                        data.tab[i][j] = foesFigure;
                         gameRules.checkEnd();
                         if (this.gameRules.gameOver) {
                             data.tab[i][j] = figure;
@@ -64,5 +72,53 @@ public class PlayerAI extends Player {
                 }
             }
         }
+    }
+    private boolean is3InAColumn(Data data) {
+        for (int y = 1; y < data.tab.length; y++) {
+            for (int x = 1; x < data.tab.length - 1; x++) {
+                if (data.tab[x][y].equals(foesFigure)) {
+                    if (data.tab[x + 1][y].equals(foesFigure)) {
+                        if (data.tab[x + 2][y].equals(foesFigure)) {
+                            if (data.tab[x - 1][y].equals(Figures.EMPTY)) {
+                                possibleX = x - 1;
+                                possibleY = y;
+                                return true;
+                            } else {
+                                possibleX = x + 3;
+                                possibleY = y;
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    private boolean is3InARow(Data data) {
+        for (int y = 1; y < data.tab.length; y++) {
+            for (int x = 1; x < data.tab.length - 1; x++) {
+                if (data.tab[x][y].equals(foesFigure)) {
+                    if (data.tab[x][y + 1].equals(foesFigure)) {
+                        if (data.tab[x][y + 2].equals(foesFigure)) {
+                            if (data.tab[x][y - 1].equals(Figures.EMPTY)) {
+                                possibleX = x;
+                                possibleY = y - 1;
+                                return true;
+                            } else {
+                                possibleX = x;
+                                possibleY = y + 3;
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    private void counterAttack(int possibleX , int possibleY, Data data) {
+        data.tab[possibleX][possibleY] = figure;
+        System.out.println("Komputer kontratakuje!");
     }
 }

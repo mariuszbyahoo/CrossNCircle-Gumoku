3ba887483b935123e093d969370f0222233aaf42
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-01-31 18:14:00 +0100

    Setting the propper window size

--- src/pl/budzisz/mariusz/cross_n_circle/view/CrossNCircleButton.java
+++ src/pl/budzisz/mariusz/cross_n_circle/view/CrossNCircleButton.java
@@ -1,36 +1,29 @@
 package pl.budzisz.mariusz.cross_n_circle.view;
 
-import pl.budzisz.mariusz.cross_n_circle.figures.Figures;
-
 import javax.swing.*;
 
 public class CrossNCircleButton extends JButton {
-    int x;
-    int y;
-    public String symbol;
+    int arrayPositionX;
+    int arrayPositionY;
 
-
-    public CrossNCircleButton(int x, int y){
-        super();
-        symbol = Figures.EMPTY.getSymbol();
-        setText(symbol);
-        setX(x);
-        setY(y);
+    public CrossNCircleButton(int arrayPositionX, int arrayPositionY){
+        setArrayPositionX(arrayPositionX);
+        setArrayPositionY(arrayPositionY);
     }
 
-    public int getX() {
-        return x;
+    public int getArrayPositionX() {
+        return arrayPositionX;
     }
 
-    public int getY() {
-        return y;
+    public int getArrayPositionY() {
+        return arrayPositionY;
     }
 
-    public void setY(int y) {
-        this.y = y;
+    public void setArrayPositionY(int y) {
+        this.arrayPositionY = y;
     }
 
-    public void setX(int x) {
-        this.x = x;
+    public void setArrayPositionX(int x) {
+        this.arrayPositionX = x;
     }
 }

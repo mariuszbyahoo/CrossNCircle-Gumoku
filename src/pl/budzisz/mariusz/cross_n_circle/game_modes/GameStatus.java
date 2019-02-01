aa45dc37757b2dd0a5643d97bb96e7fa360f41be
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-02-01 09:33:29 +0100

    Creating the GameStatus enum

---
+++ src/pl/budzisz/mariusz/cross_n_circle/game_modes/GameStatus.java
@@ -0,0 +1,14 @@
+package pl.budzisz.mariusz.cross_n_circle.game_modes;
+
+public enum GameStatus {
+    IN_PROGRESS("In progress"),
+    X_WON("X Won!"),
+    O_WON("O Won!"),
+    DRAW("Draw");
+
+    private String desc;
+
+    GameStatus(String desc) {
+        this.desc = desc;
+    }
+}
